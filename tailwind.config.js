1b0abb25b957ffcb99bcd8b5c1e041b09687fe04
Guilherme Quincó <130392565+guiguhguile@users.noreply.github.com>
2024-03-20 17:51:18 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "bg-hero":
+          "url('https://images.unsplash.com/photo-1533563906091-fdfdffc3e3c4?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      },
+    },
+  },
+  plugins: [],
+};
